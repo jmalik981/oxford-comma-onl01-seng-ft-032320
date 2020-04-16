332b2d6022f24af78f5299f1f262d9db18a3abde
Junaid Malik <jmalik981@gmail.com>
2020-04-16 03:36:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,16 @@
 def oxford_comma(array)
+  
 if array.length == 2 
   return "#{array[0]} and #{array[1]}"
 end
+
 if array.length == 3
   return "#{array[0]}, #{array[1]}, and #{array[2]}"
 end
+
+if array.length>3
+  return array.join(",")
+end
+
 array.join
 end
